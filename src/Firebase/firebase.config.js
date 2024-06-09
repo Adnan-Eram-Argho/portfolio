5b171736a4c35cc2d6c955af7bf82d54240c5f50
Adnan-Eram-Argho <adnaneramargho@gmail.com>
2024-06-09 15:38:12 +0600

    Committing changes before reverting

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -10,14 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_FIREBASE_APPID,
 };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCFEAeRzfzjdR6suGsMP5seUdEKHGxxAXM",
-//     authDomain: "portfolio-b7fcc.firebaseapp.com",
-//     projectId: "portfolio-b7fcc",
-//     storageBucket: "portfolio-b7fcc.appspot.com",
-//     messagingSenderId: "665902183330",
-//     appId: "1:665902183330:web:5405ba98782edb1097b8fb"
-//   };
+
 
 const app = initializeApp(firebaseConfig);
 
